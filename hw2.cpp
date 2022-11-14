b4106149a82ca630041001be108068ac75ec24d5
yanjiarui173 <1954738284@qq.com>
2022-11-14 22:38:14 +0800

    text commit

---
+++ hw2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+	unsigned long ulsum =0;
+	unsigned long ulcount=5;
+	while (0<=ulcount){
+		ulsum +=ulcount;
+		ulcount--;
+	}
+	printf("%lu\n",ulsum);
+	return 0;
+}
---
+++ hw3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main (){
+	char name[10];
+	int var1=1,var2=2;
+	printf("var1=%d,var2=%d\n",var1,var2);
+	puts("where do you live?");
+	gets(name);
+	printf("<%s> is length %d\n",var1,var2);
+	puts("ok");
+	return 0;
+	
+	
+	
+}
